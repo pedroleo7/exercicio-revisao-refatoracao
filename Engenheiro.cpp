69becac822ff3086030bb1ec482fad9aeeac293b
Pedro Leonardo <64475910+pedroleo7@users.noreply.github.com>
2020-10-20 17:55:13 -0300

    Update Engenheiro.cpp
    mudei as chamadas e implementei as funções necessárias

--- Engenheiro.cpp
+++ Engenheiro.cpp
@@ -1,11 +1,32 @@
 #include <string>
 #include "Empregado.hpp"
+#include "Engenheiro.hpp" 
 
 class Engenheiro : public Empregado {
 
+//modfiquei os operadores e implementei gets
   public:
-    std::string nome;  
-	int projetos;
+ Engenheiro( std::string nome, double salarioH, int projetos, double Hrtraba){
+      setNome(nome);
+      setSalarioHora(salarioH);
+      _projetos = projetos;
+      set_horasTrabalhadas(Hrtraba); 
+}
+  void imprime(){
+    std::cout << "Nome: " << getNome << std::endl;
+  std::cout << "Salario Mes: " << getsalarioMes<< std::endl;
+  std::cout << "Projetos: " << _projetos << std::endl;
+  std::cout << std::endl;
+  }
+  
+    int getProjetos(){
+      return _projetos;
+    }
+
+    private:  
+	  int _projetos;
+    
 	
 };
+
 
