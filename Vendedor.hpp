9237ccffb72f5a5af33a7096c6d146addb59d9b9
Pedro Leonardo <64475910+pedroleo7@users.noreply.github.com>
2020-10-20 17:50:12 -0300

    Create Vendedor.hpp
    implementei o contrato do vendedor

---
+++ Vendedor.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include "Empregado.hpp"
+
+class Vendedor : public Empregado {
+
+  public:
+    Vendedor(std::string nome, double salhora, double quota);
+
+    double quotaTotalAnual();
+    void imprime();
+
+    
+
+
+  private:
+	  
+	
+	
+};
